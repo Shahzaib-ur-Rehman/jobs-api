9818578832352dc706b68f3018b5f32cd6d68b5b
Shahzaib-ur-Rehman <shahzaib@hq.crunchcraft.com>
2024-04-24 18:56:37 +0500

    security packages added

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const cors = require('cors')
+const helmet = require("helmet");
+const xss = require("xss-clean")
+const rateLimiter= require("express-rate-limit")
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -16,8 +19,14 @@ const jobsRouter = require("./routes/jobs")
 
 //auth middleware
 const auth = require("./middleware/authentication")
-
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(xss())
+app.use(cors())
 // extra packages
 
 // routes
